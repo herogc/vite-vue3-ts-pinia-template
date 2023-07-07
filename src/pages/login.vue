81ccd722c4d54888629fda7101a9c8cbc8c25d5c
guchao <skyblue0828@outlook.com>
2023-07-07 17:59:54 +0800

    项目初始化

---
+++ src/pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h5>{{ userStore.name }}</h5>
+    <h3>Mouse: {{x}} x {{y}}</h3>
+    <a-button type="primary">Primary Button</a-button>
+  </div>
+  
+</template>
+<script lang="ts" setup>
+import { useMouse } from '@vueuse/core'
+import { useUserStore } from '../store/user'
+const userStore = useUserStore()
+userStore.updateName('李四')
+const { x, y } = useMouse()
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path';
+
+// 自动导入vue中hook reactive ref等
+import AutoImport from "unplugin-auto-import/vite"
+//自动导入ui-组件 比如说ant-design-vue  element-plus等
+import Components from 'unplugin-vue-components/vite';
+//ant-design-vue
+import {AntDesignVueResolver} from "unplugin-vue-components/resolvers"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    //设置别名
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      //安装两行后你会发现在组件中不用再导入ref，reactive等
+      imports: ['vue', 'vue-router'],
+      dts: "src/auto-import.d.ts",
+      resolvers: [AntDesignVueResolver()]
+    }),
+    Components({
+      //ant-design-vue   importStyle = false 样式就没了
+        resolvers: [AntDesignVueResolver({importStyle: true, resolveIcons: true})],
+    })
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 8080, //启动端口
+    hmr: {
+        host: '127.0.0.1',
+        port: 8080
+    },
+    // 设置 https 代理
+    proxy: {
+        '/api': {
+            target: 'https://qylweb.bda.gov.cn',
+            changeOrigin: true,
+            rewrite: (path: string) => path.replace(/^\/api/, '')
+        }
+    }
+}
+})
